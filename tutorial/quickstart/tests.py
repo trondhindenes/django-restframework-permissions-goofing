95ae641b89b9738a87c960445146c65dea14a28e
Trond Hindenes <trond@hindenes.com>
2022-06-18 10:16:03 +0200

    remove some djunk

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -13,7 +13,7 @@ from quickstart import permission_rules
 class TaskTest(TestCase):
     def setUp(self):
         from django.conf import settings
-        settings.DEBUG=True
+        settings.DEBUG = True
 
     def tearDown(self):
         from django.db import connection
@@ -23,40 +23,21 @@ class TaskTest(TestCase):
     def test_direct_permissions(self):
         group1 = Group.objects.create(name="group1")
         user1 = User.objects.create(username="user1")
-
-        #user1.groups.add(group1)
-
-        #user2 = User.objects.create(username="user2")
-
         task_group1 = TaskGroup.objects.create(name="group1", owned_by_group=group1)
         task1 = Task.objects.create(
             summary="what", content="what", task_group=task_group1, reported_by=user1
         )
-
-        # self.assertFalse(user1.has_perm("assign_task", task1))
-
-        # assign_perm('assign_task', user1, task1)
         self.assertTrue(user1.has_perm("quickstart.change_task", task1))
 
     def test_direct_group_permissions(self):
         group1 = Group.objects.create(name="group1")
         user1 = User.objects.create(username="user1")
-
         user1.groups.add(group1)
-
-        user2 = User.objects.create(username="user2")
-
         task_group1 = TaskGroup.objects.create(name="group1", owned_by_group=group1)
         task1 = Task.objects.create(
             summary="what", content="what", task_group=task_group1, reported_by=user1
         )
-
-        # self.assertFalse(user1.has_perm("assign_task", task1))
-
-        # assign_perm(perm="assign_task", user_or_group=group1, obj=task1)
         self.assertTrue(user1.has_perm("quickstart.change_task", task1))
-
-        # self.assertFalse(user2.has_perm("assign_task", task1))
 
     # noinspection DuplicatedCode
     def test_indirect_group_permissions(self):
@@ -75,8 +56,6 @@ class TaskTest(TestCase):
 
         # user2 has access to the task via the task group/group
         self.assertTrue(user2.has_perm("quickstart.change_task", task1))
-
-        # self.assertFalse(user2.has_perm("assign_task", task1))
 
     def test_indirect_group_permissions_noaccess(self):
         group1 = Group.objects.create(name="group1")
